25d6a5aabdbfacd36132f194276a6a3fa3e27cfe
debian <debian@localhost.localdomain>
2025-03-08 17:03:04 -0300

    Agregado formulario de login y enlace en el Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,13 @@ const Navbar = () => {
           <Link to="/about">Acerca de</Link>
         </li>
         <li>
-          <Link to="/contact">Contacto</Link> {/* Nuevo enlace */}
+          <Link to="/contact">Contacto</Link>
+        </li>
+        <li>
+          <Link to="/login">Iniciar Sesión</Link> {/* Enlace al login */}
+        </li>
+        <li>
+          <Link to="/register">Registrarse</Link> {/* Enlace al registro */}
         </li>
       </ul>
     </nav>
