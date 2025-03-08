1911e8db799af68ec560207223bdfe591625cee2
debian <debian@localhost.localdomain>
2025-03-08 12:15:49 -0300

    Primer commit: proyecto React básico

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>¡Hola, Mundo!</h1>
+        <p>Este es mi primer proyecto en React.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
