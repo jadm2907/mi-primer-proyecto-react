dd89dfeacc84a4d160d4d0e53ba8199ee47d6886
debian <debian@localhost.localdomain>
2025-03-08 17:24:33 -0300

    Corrección tema claro/oscuro

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,9 +1,20 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
   const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // Efecto para cambiar la clase del body
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add('dark-mode');
+      document.body.classList.remove('light-mode');
+    } else {
+      document.body.classList.add('light-mode');
+      document.body.classList.remove('dark-mode');
+    }
+  }, [isDarkMode]);
 
   const toggleTheme = () => {
     setIsDarkMode(!isDarkMode);
@@ -15,3 +26,12 @@ export const ThemeProvider = ({ children }) => {
     </ThemeContext.Provider>
   );
 };
+  /*const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );*/
